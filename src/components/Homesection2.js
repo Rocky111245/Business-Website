e9a016c2e244bbf2c36981a13282dfed26678dac
Rocky111245 <rakibalhasan170@gmail.com>
2024-05-07 14:45:32 +0800

    Moving to Products Page Responsiveness

--- src/components/Homesection2.js
+++ src/components/Homesection2.js
@@ -12,35 +12,35 @@ const Homesection2 = () => {
 
         <div className='Homesection2_blocks'>
           <a href="/Products" className='icon-link' >
-            <FontAwesomeIcon icon={faEye} size="10x" color='#4169E1' />
+            <FontAwesomeIcon className='icon' icon={faEye}  color='#4169E1' />
             <div className='Homesection2_card_title'>Ophthalmology</div>
           </a>
         </div>
 
         <div className='Homesection2_blocks'>
           <a href="/Products" className='icon-link'>
-            <FontAwesomeIcon icon={faTooth} size="8x" color='#4169E1' />
+            <FontAwesomeIcon className='icon' icon={faTooth}  color='#4169E1' />
             <div className='Homesection2_card_title'>Dental Treatment Delivery System</div>
           </a>
         </div>
 
         <div className='Homesection2_blocks'>
           <a href="/Products" className='icon-link'>
-            <FontAwesomeIcon icon={faHeartbeat} size="10x" color='#4169E1' />
+            <FontAwesomeIcon className='icon' icon={faHeartbeat}color='#4169E1' />
             <div className='Homesection2_card_title'>ICU Equipment</div>
           </a>
         </div>
 
         <div className='Homesection2_blocks'>
           <a href="/Products" className='icon-link'>
-            <FontAwesomeIcon icon={faBabyCarriage} size="10x" color='#4169E1' />
+            <FontAwesomeIcon className='icon' icon={faBabyCarriage}  color='#4169E1' />
             <div className='Homesection2_card_title'>Neonatal/Pediatric</div>
           </a>
         </div>
 
         <div className='Homesection2_blocks'>
           <a href="/Products" className='icon-link'>
-            <GiKidneys size="150px" color='#4169E1' />
+            <GiKidneys className='icon'  color='#4169E1' />
             <div className='Homesection2_card_title'>Nephrology</div>
           </a>
         </div>
