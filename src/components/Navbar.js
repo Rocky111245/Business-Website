8de3b9a4feba5fa72e70c3bb31a39900ed163f40
Rocky111245 <rakibalhasan170@gmail.com>
2024-05-07 09:35:24 +0800

    Adding Responsiveness (Small Laptops)-Part 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,39 +2,29 @@
 import React,{useState} from 'react';
 import './Navbar.css';
 import { RxChevronRight } from "react-icons/rx";
-import '../utilities/fonts/fonts.css';
 
 
 
 const Navbar = () => {
- 
   return (
-    <nav className="navbar">
-  <div className="navbar__logo-title-container">
-    <img src="https://ik.imagekit.io/emtbd/emt_images/emt_logo/EMT-green.png?ik-sdk-version=javascript-1.4.3&updatedAt=1673822808512" className="navbar__logo" />
-    <h1 className="navbar__title">Evolution Medical Technologies</h1>
-    <h4 class="moto__text">Medical Technology.Delivered.</h4>
-  </div>
-  <ul className="navbar__menu">
-    <li className="navbar__menu-item">
-      <a href="/" className="navbar__menu-link" >Home</a>
-    </li>
-  <li className="navbar__menu-item">
-      <a href="/Products" className="navbar__menu-link navbar__menu-link-product">
-    Products 
-  </a>
-</li>
-
-    <li className="navbar__menu-item">
-      <a href="/About_Us" className="navbar__menu-link">About Us</a>
-    </li>
-    <li className="navbar__menu-item">
-      <a href="/Contact_Us" className="navbar__menu-link">Contact Us</a>
-    </li>
-  </ul>
-</nav>
-
-  )
+    <header className="navbar">
+      <div className="navbar__logo-title">
+        <img src="https://ik.imagekit.io/emtbd/emt_images/emt_logo/EMT-green.png?ik-sdk-version=javascript-1.4.3&updatedAt=1673822808512" alt="EMT Logo" className="navbar__logo" />
+        <div className="navbar__title-container">
+          <h1 className="navbar__title">Evolution Medical Technologies</h1>
+          <h4 className="navbar__motto">Medical Technology. Delivered.</h4>
+        </div>
+      </div>
+      <nav className="navbar__menu">
+        <ul className="navbar__menu-list">
+          <li className="navbar__menu-item"><a href="/" className="navbar__menu-link">Home</a></li>
+          <li className="navbar__menu-item"><a href="/Products" className="navbar__menu-link">Products</a></li>
+          <li className="navbar__menu-item"><a href="/About_Us" className="navbar__menu-link">About Us</a></li>
+          <li className="navbar__menu-item"><a href="/Contact_Us" className="navbar__menu-link">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
--- src/components/SubFooter.js
+++ src/components/SubFooter.js
@@ -5,13 +5,16 @@ const SubFooter = () => {
     return (
         <div className="subfooter__container">
             <img
-                className='subfooter__image'
-                src='https://ik.imagekit.io/emtbd/emt_images/images/hans-reniers-lQGJCMY5qcM-unsplash.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1673968939541'
+                className="subfooter__image"
+                src="https://ik.imagekit.io/emtbd/emt_images/images/hans-reniers-lQGJCMY5qcM-unsplash.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1673968939541"
                 alt="Company Image"
             />
             <div className="subfooter__sub-container">
                 <h3 className="subfooter__title">Download Our Company Profile</h3>
-                <a href="https://ik.imagekit.io/emtbd/emt_images/EMT%20Profile.pdf?updatedAt=1714984693242" target="_blank" rel="noopener noreferrer" className="subfooter__button-link">
+                <a href="https://ik.imagekit.io/emtbd/emt_images/EMT%20Profile.pdf?updatedAt=1714984693242" 
+                   target="_blank" 
+                   rel="noopener noreferrer" 
+                   className="subfooter__button-link">
                     <button className="subfooter__button">Download EMT Profile</button>
                 </a>
             </div>
