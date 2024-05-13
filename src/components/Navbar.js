fc8c873562878435b00913058f4ecc60543c43ee
Rocky111245 <rakibalhasan170@gmail.com>
2024-05-13 10:34:42 +0800

    Refining Product Section
    Added Dropdown Menu for Product Section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
-
-import React,{useState} from 'react';
+import React from 'react';
 import './Navbar.css';
 import { RxChevronRight } from "react-icons/rx";
 
-
+import { GoEye } from "react-icons/go";
+import { TbDental } from "react-icons/tb";
+import { BsLungs } from "react-icons/bs";
+import { FaBaby } from "react-icons/fa";
+import { GiKidneys } from "react-icons/gi";
 
 const Navbar = () => {
   return (
@@ -18,7 +21,22 @@ const Navbar = () => {
       <nav className="navbar__menu">
         <ul className="navbar__menu-list">
           <li className="navbar__menu-item"><a href="/" className="navbar__menu-link">Home</a></li>
-          <li className="navbar__menu-item"><a href="/Products" className="navbar__menu-link">Products</a></li>
+          <li className="navbar__menu-item">
+            <div className="navbar__menu-link-container product ">
+            <a className="navbar__menu-link ">
+              Products
+            </a>
+            <RxChevronRight className="chevron" />
+            </div>
+            <ul className="dropdown-menu">
+                <ul><a href="/Ophthalmology"><GoEye className="icon" />Ophthalmology</a></ul>
+                <ul><a href="/Dental-Treatment"><TbDental className="icon" />Dental Treatment Delivery System</a></ul>
+                <ul><a href="/ICU-Equipment"><BsLungs className="icon" />ICU Equipment</a></ul>
+                <ul><a href="/Neonatal-Pediatric"><FaBaby className="icon" />Neonatal/Pediatric</a></ul>
+                <ul><a href="/Nephrology"><GiKidneys className="icon" />Nephrology</a></ul>
+            </ul>
+
+          </li>
           <li className="navbar__menu-item"><a href="/About_Us" className="navbar__menu-link">About Us</a></li>
           <li className="navbar__menu-item"><a href="/Contact_Us" className="navbar__menu-link">Contact Us</a></li>
         </ul>
